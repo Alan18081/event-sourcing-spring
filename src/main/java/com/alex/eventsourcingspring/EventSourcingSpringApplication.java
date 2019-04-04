e9bd56fe496ce47a2f1e1033285c2c5b4bc4017f
t.kandibolotskaya <t.kandibolotskaya@bet-makers.com>
2019-04-04 19:15:14 +0300

    initial commit

---
+++ src/main/java/com/alex/eventsourcingspring/EventSourcingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alex.eventsourcingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingSpringApplication.class, args);
+	}
+
+}
